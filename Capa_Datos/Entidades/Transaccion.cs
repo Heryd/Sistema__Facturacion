0b9b64574221d38eeeadf6a8de33cc427259e076
Morla Gordillo Heryd Xavier <silla0408@hotmail.com>
2023-08-18 21:54:27 -0500

    agregada clase transaccion y gerente

--- Capa_Datos/Entidades/Transaccion.cs
+++ Capa_Datos/Entidades/Transaccion.cs
@@ -7,12 +7,28 @@ using System.Threading.Tasks;
 namespace Capa_Datos.Entidades
 {
     /// <summary>
-    /// Clase Transaccion
+    /// Esta clase es un ejemplo para verificar el trabajo colaborativo mediante 
+    /// el uso de la herramienta git, para mantener un control de versiones del proyecto
     /// </summary>
     public class Transaccion
     {
+        #region Descripción del identificador de la transacción
+        /// <summary>
+        /// Método <b>bId</b> obtiene el identificador de la transacción.
+        /// </summary> 
+        #endregion
         public int Id { get; set; }
+        #region Descripción del identificador del pago
+        /// <summary>
+        /// Método <b>bIdPago</b> obtiene el identificador del pago realizado.
+        /// </summary> 
+        #endregion
         public int IdPago { get; set; }
+        #region Descripción del método del pago
+        /// <summary>
+        /// Método <b>MetodoPago</b> obtiene el métod de pago con el que se realizó la transacción.
+        /// </summary> 
+        #endregion
         public string MetodoPago { get; set; }
     }
 
