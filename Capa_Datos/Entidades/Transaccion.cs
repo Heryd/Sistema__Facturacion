f24f1dd7abc300fa2cd68762d70fdfeff363cafc
Morla Gordillo Heryd Xavier <silla0408@hotmail.com>
2023-08-18 20:47:47 -0500

    cambios de emergencia

--- Capa_Datos/Entidades/Transaccion.cs
+++ Capa_Datos/Entidades/Transaccion.cs
@@ -1,10 +1,15 @@
-﻿namespace Capa_Datos.Entidades
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capa_Datos.Entidades
 {
     internal class Transaccion
     {
         public int Id { get; set; }
         public int IdPago { get; set; }
         public string MetodoPago { get; set; }
-        public int IdFactura { get; set; }
     }
 }
