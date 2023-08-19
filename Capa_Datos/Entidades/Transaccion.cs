b78fdf9de2196c9d95050253013ba7e26dabcd65
Valencia Angelo <Valencia Angelo@DESKTOP-BI9MGEN>
2023-08-18 20:56:02 -0500

    cambio de clase

--- Capa_Datos/Entidades/Transaccion.cs
+++ Capa_Datos/Entidades/Transaccion.cs
@@ -8,8 +8,6 @@ namespace Capa_Datos.Entidades
 {
     internal class Transaccion
     {
-        public int Id { get; set; }
-        public int IdPago { get; set; }
-        public string MetodoPago { get; set; }
+      
     }
 }
